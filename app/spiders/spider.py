526e95ddfa5c669d9d7a77303ec387c73bc77736
zjw <zjw0579@gmail.com>
2018-05-14 13:30:23 +0800

    update cookie

--- app/spiders/spider.py
+++ app/spiders/spider.py
@@ -11,10 +11,9 @@ def get_html(url, count=1):
     global cookie, redis
     max_count = int(MAX_REQUEST_COUNT)
     headers = {
+        'Cookie': cookie,
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36',
     }
-    if cookie:
-        headers['Cookie'] = cookie
     print('Try Count', count)
     if count >= max_count:
         print('Tried Too Many Count')
