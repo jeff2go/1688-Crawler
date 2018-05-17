c0c2ba7984d66157a06ba64d23fd8437f2f7a392
zjw <zjw0579@gmail.com>
2018-05-17 14:32:39 +0800

    update headers

--- app/spiders/spider.py
+++ app/spiders/spider.py
@@ -13,6 +13,9 @@ def get_html(url, count=1):
     headers = {
         'Cookie': cookie,
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1 Safari/605.1.15',
+        'Connection': 'keep-alive',
+        'Accept-Language': 'zh-cn',
+        'Accept-Encoding': 'br, gzip, deflate',
     }
     print('Try Count', count)
     if count >= max_count:
