c700c21c1f0e883b48331b67dcd2d920ccb26b1a
zjw <zjw0579@gmail.com>
2018-05-17 14:48:00 +0800

    fix typo

--- app/spiders/spider.py
+++ app/spiders/spider.py
@@ -13,7 +13,7 @@ def get_html(url, count=1):
     headers = {
         'Cookie': cookie,
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
-        'upgrade-insecure-requests': 1,
+        'upgrade-insecure-requests': '1',
     }
     print('Try Count', count)
     if count >= max_count:
