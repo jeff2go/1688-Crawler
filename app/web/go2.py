72b355f440c9567b71206f08795be1a1f92633a5
JeffreyChu <zjw0579@gmail.com>
2019-09-27 10:41:22 +0800

    update go2.cn产品抓取

--- app/web/go2.py
+++ app/web/go2.py
@@ -3,20 +3,6 @@ import traceback
 
 from . import web
 from app.spiders.go2product import Go2Product
-from app.spiders.products import Products
-
-
-@web.route('/go2/products', methods=['GET'])
-def crawl_go2_products():
-    try:
-        products = Products()
-        products = products.go(request.args.get('url'))
-        return jsonify(products)
-    except Exception as e:
-        traceback.print_exc()
-        resp = jsonify({'errcode': 500, 'errmsg': '抓取产品列表异常: ' + str(e)})
-        resp.status_code = 500
-        return resp
 
 
 @web.route('/go2/product', methods=['GET'])
