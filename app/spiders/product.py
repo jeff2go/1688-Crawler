523bce3f32ca880eae288169ef3910795e5734c8
jeff2go <zjw0579@gmail.com>
2023-07-31 10:26:21 +0800

    feat: update product fetch

--- app/spiders/product.py
+++ app/spiders/product.py
@@ -22,13 +22,12 @@ headers = {
 }
 
 class Product:
-    # 过滤产品，可导入义乌购的返回 errcode: 0
     def __filter(self, content):
         content = re.findall("<span>预估生产周期</span>", content)
         if len(content) > 0:
             return {
                 'errcode': 101,
-                'errmsg': '此产品为淘工厂类型，暂时无法导入义乌购产品体系'
+                'errmsg': '此产品为淘工厂类型，暂不支持抓取'
             }
         return {
             'errcode': 0
@@ -36,66 +35,19 @@ class Product:
 
     # 基本信息 & SKU
     def __extract_base_and_sku(self, tree):
-        script = tree.xpath('//script[contains(., "var iDetailConfig = ")]/text()')[0]
+        script = tree.xpath('//script[contains(., "window.__INIT_DATA")]/text()')[0]
 
-        base_str = re.findall("var iDetailConfig = ({[\s\S]*?});", script)[0].replace("'", '"')
-        data = {k: v for k, v in json.loads(base_str).items() if
-                k in ['offerid', 'unit', 'isRangePriceSku', 'isSKUOffer', 'beginAmount', 'refPrice', 'companySiteLink',
-                      'isTp', 'mkcActivityId']}
-        sku_str = re.findall("var iDetailData = ({[\s\S]*?});", script)[0]
-        data.update(json.loads(sku_str.replace("'", '"')))
+        base_str = re.findall("window.__INIT_DATA=({[\s\S]*})", script)[0].replace('\"', '"')
 
-        return data
-
-    # 阶梯价
-    def __extract_price_range(self, tree):
-        items = tree.xpath('//td[@data-range]')
-        price_range = []
-        for item in items:
-            price_range_item = json.loads(item.attrib['data-range'])
-            price_range.append([int(price_range_item['begin']), float(price_range_item['price'])])
-        return price_range
-
-    # 阶梯价的可订购数量
-    def __extract_can_book_count_based_on_price_range(self, tree):
-        text_elements = tree.xpath('//div[contains(@class, "obj-amount")]//span[@class="total"]/text()')
-        if len(text_elements) > 0:
-            res = re.findall('\d+', text_elements[0])
-            if len(res) > 0:
-                return res[0]
-        return 100
-
-    # 标题
-    def __extract_title(self, tree):
-        return tree.xpath('//h1[@class="d-title"]/text()')[0]
+        return json.loads(base_str)['globalData']
 
     # 图片
     def __extract_images(self, tree):
-        image_elements = tree.xpath('//div[@id="dt-tab"]//li[contains(@class, "tab-trigger")]')
-        return list(map(lambda img_element: json.loads(img_element.attrib['data-imgs'])['original'], image_elements))
-
-    # 规格参数
-    def __extract_attributes(self, tree):
-        attribute_features = tree.xpath('//td[@class="de-feature"]/text()')
-        attribute_values = tree.xpath('//td[@class="de-value"]/text()')
-        return list(
-            map(lambda feature, value: {'feature': feature, 'value': value}, attribute_features, attribute_values))
-
-    # 详情描述
-    def __extract_description(self, tree):
-        description_request_url = tree.xpath('//div[@id="desc-lazyload-container"]')[0].attrib['data-tfs-url']
-        content = get_html(description_request_url, headers)
-        content = content[30:-3].replace('\\', '')
-        content = re.sub('href[^>]+', 'href="#none"', content)
-        if not content:
-            easy_desc = tree.xpath('//div[contains(@class, "offerdetail_easyoffer_dsc")]')[0]
-            content += str(html.tostring(easy_desc), encoding="utf-8")
-        return content
+        return [item["fullPathImageURI"] for item in tree["images"]]
 
     def go(self, url):
         content = get_html(url, headers)
 
-        # 过滤不符合义乌购加个体系的产品
         filter_result = self.__filter(content)
         if filter_result['errcode'] != 0:
             return filter_result
@@ -103,17 +55,12 @@ class Product:
         tree = html.fromstring(content)
 
         product = self.__extract_base_and_sku(tree)
-        if product['isSKUOffer'] == 'false':
-            # 抓取 阶梯价
-            price_range = self.__extract_price_range(tree)
-            if len(price_range) > 0:
-                product['isRangePriceSku'] = 'true'
-                can_book_count = self.__extract_can_book_count_based_on_price_range(tree)
-                product['sku'] = {"priceRange": price_range, "skuProps": [], "canBookCount": str(can_book_count)}
-
-        product['title'] = self.__extract_title(tree)
-        product['images'] = self.__extract_images(tree)
-        product['attributes'] = self.__extract_attributes(tree)
-        product['description'] = self.__extract_description(tree)
-
-        return product
+        result = dict()
+        result['title'] = product["tempModel"]["offerTitle"]
+        result["companyName"] = product["tempModel"]["companyName"]
+        result["unit"] = product["tempModel"]["offerUnit"]
+        result['images'] = self.__extract_images(product)
+        result["order"] = product["orderParamModel"]["orderParam"]
+        result["sku"] = product["skuModel"]
+        # todo 商品属性 商品描述
+        return result
